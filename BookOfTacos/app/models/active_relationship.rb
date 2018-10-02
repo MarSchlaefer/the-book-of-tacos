cbfbcb471fdead5f5ddea1e2d68085ae20a2199f
yammik <mkim0514@gmail.com>
2018-10-02 17:57:56 -0400

    adds Follow button on user show page

--- BookOfTacos/app/models/active_relationship.rb
+++
@@ -1,3 +0,0 @@
-class ActiveRelationship < ApplicationRecord
-  
-end
--- BookOfTacos/config/routes.rb
+++ BookOfTacos/config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   resources :recipes
   resources :comments
   resources :posts
-  resources :passive_relationships
-  resources :active_relationships
   resources :users, except: %i(index)
   resources :users do
     member do
@@ -14,5 +12,7 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :relationships, only: %i(create destroy)
 
 end
